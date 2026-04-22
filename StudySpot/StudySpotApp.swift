d7d706364d38c6e89ea1eb2a2f3d5947c199419d
Aidan McGinty <ajmcginty1@gmail.com>
2026-04-22 09:58:06 -0400

    Initial Commit

---
+++ StudySpot/StudySpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySpotApp.swift
+//  StudySpot
+//
+//  Created by Aidan McGinty on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct StudySpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
